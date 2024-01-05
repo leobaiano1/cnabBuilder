e30519e95a0bfb3a09bf12609aef7fcb02444e21
Leo Baiano <leo.baiano@copilotu.ai>
2024-01-05 11:46:51 -0300

    feat: resultado do teste

--- cnabRows.js
+++ cnabRows.js
@@ -11,14 +11,21 @@ const optionsYargs = yargs(process.argv.slice(2))
   .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
   .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
   .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
+  .option("c", { alias: "cnab", describe: "caminho do arquivo CNAB", type: "string" } )
+  .option("e", { alias: "empresa", describe: "Nome da empresa", type: "string", demandOption: true })
   .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
   .argv;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const file = path.resolve(`${__dirname}/cnabExample.rem`)
 
-const { from, to, segmento } = optionsYargs
+let cnab;
+
+cnab = optionsYargs.cnab || path.join(__dirname, 'cnabExample.rem');
+
+const file = path.resolve(cnab)
+
+const { from, to, segmento, empresa } = optionsYargs
 
 const sliceArrayPosition = (arr, ...positions) => [...arr].slice(...positions)
 
@@ -52,22 +59,50 @@ readFile(file, 'utf8')
     const cnabTail = sliceArrayPosition(cnabArray, -2)
 
     if (segmento === 'p') {
-      log(messageLog(cnabBodySegmentoP, 'P', from, to))
+      filterByName(cnabBodySegmentoP, 'P', from, to);
       return
     }
 
     if (segmento === 'q') {
-      log(messageLog(cnabBodySegmentoQ, 'Q', from, to))
+      filterByName(cnabBodySegmentoQ, 'Q', from, to);
       return
     }
 
     if (segmento === 'r') {
-      log(messageLog(cnabBodySegmentoR, 'R', from, to))
+      filterByName(cnabBodySegmentoR, 'R', from, to);
       return
     }
+
+    const result = {
+      header: cnabHeader,
+      body: {
+        P: cnabBodySegmentoP,
+        Q: cnabBodySegmentoQ,
+        R: cnabBodySegmentoR,
+      },
+      tail: cnabTail,
+    };
+
+
+    const jsonResult = JSON.stringify(result, null, 2);
+    return writeFile('output.json', jsonResult, 'utf8');
 
   })
   .catch(error => {
     console.log("🚀 ~ file: cnabRows.js ~ line 76 ~ error", error)
   })
+
+  function filterByName(segmento, segmentoType, from, to) {
+    if (!empresa) {
+      log(messageLog(segmento, segmentoType, from, to));
+    } else {
+      const filteredLines = segmento.split('\n').filter(line => line.includes(empresa));
+      if (filteredLines.length > 0) {
+        log(messageLog(filteredLines.join('\n'), segmentoType, from, to));
+      } else {
+        console.log(`Nenhuma linha correspondente para a empresa "${empresa}" no segmento ${segmentoType}`);
+      }
+    }
+  }
+
 console.timeEnd('leitura Async')
